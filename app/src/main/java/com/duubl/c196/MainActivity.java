8c93f991b4cc847105c625b08e726f5b62643e0a
Duubl <147439692+Duubl@users.noreply.github.com>
2025-01-07 07:14:15 -0800

    Added home page to navigation

--- app/src/main/java/com/duubl/c196/MainActivity.java
+++ app/src/main/java/com/duubl/c196/MainActivity.java
@@ -2,6 +2,7 @@ package com.duubl.c196;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -13,10 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-    public DrawerLayout drawerLayout;
-    public ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,27 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    Toast.makeText(MainActivity.this, "Home Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.nav_terms) {
+                    Toast.makeText(MainActivity.this, "Terms Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.nav_courses) {
+                    Toast.makeText(MainActivity.this, "Courses Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.nav_instructors) {
+                    Toast.makeText(MainActivity.this, "Instructors Selected", Toast.LENGTH_SHORT).show();
+                } else if (itemId == R.id.nav_assessments) {
+                    Toast.makeText(MainActivity.this, "Assessments Selected", Toast.LENGTH_SHORT).show();
+                }
+                drawerLayout.closeDrawers();  // Close the drawer after item is selected
+                return true;  // Return true to indicate the item selection has been handled
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
